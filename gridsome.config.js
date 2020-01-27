5743378bb5a7f43996bf9d912b91bb4fa7a01128
Francis Nepomuceno <francis.nepomuceno@paytm.com>
2020-01-27 12:11:31 -0500

    Added gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,6 +7,8 @@
 module.exports = {
   siteName: 'package.json-to-credits',
   siteDescription: 'Turn any package.json into movie credits',
+  siteUrl: 'https://n3ps.github.io',
+  pathPrefix: '/package-json-credits',
   plugins: [{
     use: 'gridsome-plugin-tailwindcss'
   }]
